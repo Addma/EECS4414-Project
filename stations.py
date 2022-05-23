47e88e3c8ad42995d3d944ba1dfee8069b8f1153
Adam Araujo <doorman09@hotmail.com>
2022-05-23 15:49:55 -0400

    Update stations.py

--- stations.py
+++ stations.py
@@ -1,7 +1,7 @@
 
 import requests
 
-APIkey = 'JWR4C22vHm9J8WvROqyUMKrCjH5IZjqOFFEhqVH3'
+APIkey = ''
 f = open('ev_stations.csv', 'w', encoding='utf-8')
 url = f'https://developer.nrel.gov/api/alt-fuel-stations/v1.csv?api_key={APIkey}&fuel_type=ELEC&limit=all'
 request = requests.request("GET", url)
